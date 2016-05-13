2480afa3e2426d3597e43a7ee724f0f94b66defe
preetsohal <preetsohal@live.com>
2016-05-13 00:08:39 -0700

    displayed the <p> data on page, working on dayAgo to display on page.

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,14 @@ var ourLocalData = [
     title:       'portfolio',
     category:    'test',
     author:      'Gurpreet',
-    siteUrl:   'https://gurpreet.herokuapp.com',
+    siteUrl:     'https://gurpreet.herokuapp.com',
     publishedOn: '2016-27-03',
-    body:        '<p>placeholder</p>'
+    body:        '<p>This is just an test body.this is being used as a test for body tag on html page.i am filling this body tag with some random lines. this page supposed to tell about my portfolio but i am just filling the spot. i will nopw start adding some more lines. you may not understand those lines but its ok. </p>'
+  },{
+    title:       'portfolio 2',
+    category:    'test2',
+    author:      'Gurpreet singh',
+    siteUrl:     'https://gurpreet.herokuapp.com',
+    publishedOn: '2016-13-05',
+    body:        '<p>This is just an test body.this is being used as a test for body tag on html page.i am filling this body tag with some random lines. this page supposed to tell about my portfolio but i am just filling the spot. i will nopw start adding some more lines. you may not understand those lines but its ok. </p>'
   }];
