19985be8349eb86304d4b707cf45b359671dbb3e
preetsohal <preetsohal@live.com>
2016-05-17 21:20:56 -0700

    worked on ajax and json

--- scripts/project.js
+++ scripts/project.js
@@ -1,46 +1,57 @@
 var projects = [];
 
-function Project (opts){
-  this.title = opts.title;
-  this.category = opts.category;
-  this.publishedOn = opts.publishedOn;
-  this.projectGhPages = opts.projectGhPages;
-  this.projectIcon = opts.projectIcon;
-  this.projectSourceCode = opts.projectSourceCode;
+function Project(opts) {
+    this.title = opts.title;
+    this.category = opts.category;
+    this.publishedOn = opts.publishedOn;
+    this.projectGhPages = opts.projectGhPages;
+    this.projectIcon = opts.projectIcon;
+    this.projectSourceCode = opts.projectSourceCode;
 }
 
 //render projects to the html page
 Project.prototype.toHtml = function() {
-  // var $newProjectHtml = $('section.template').clone();
-  // $newProjectHtml.find('h3').html(this.title);
-  // $newProjectHtml.find('.gh-pages').attr('href', this.projectGhPages);
-  // $newProjectHtml.find('img').attr('src', this.projectIcon);
-  // $newProjectHtml.find('p').html(this.projectDescription);
-  // $newProjectHtml.find('.source-code').attr('href', this.projectSourceCode);
-  // $newProjectHtml.removeClass('template');
-  // $newProjectHtml.find('time').html('about ' + parseInt((new Date() - new Date (this.publishedOn))/60/60/24/1000) + ' days ago');
-  // $newProjectHtml.addClass('projects-display');
-  // return $newProjectHtml;
-  this.publishedDaysAgo = 'about ' + parseInt((new Date() - new Date (this.publishedOn))/60/60/24/1000) + ' days ago';
-  console.log(parseInt((new Date() - new Date (this.publishedOn))/60/60/24/1000));
-  var $source = $('#render-projects').html();
-  var template = Handlebars.compile($source);
-  // this.classList.add('projects-display');
-  return template(this);
+    // var $newProjectHtml = $('section.template').clone();
+    // $newProjectHtml.find('h3').html(this.title);
+    // $newProjectHtml.find('.gh-pages').attr('href', this.projectGhPages);
+    // $newProjectHtml.find('img').attr('src', this.projectIcon);
+    // $newProjectHtml.find('p').html(this.projectDescription);
+    // $newProjectHtml.find('.source-code').attr('href', this.projectSourceCode);
+    // $newProjectHtml.removeClass('template');
+    // $newProjectHtml.find('time').html('about ' + parseInt((new Date() - new Date (this.publishedOn))/60/60/24/1000) + ' days ago');
+    // $newProjectHtml.addClass('projects-display');
+    // return $newProjectHtml;
+    this.publishedDaysAgo = 'about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago';
+    console.log(parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000));
+    var $source = $('#render-projects').html();
+    var template = Handlebars.compile($source);
+    // this.classList.add('projects-display');
+    return template(this);
 };
+Project.fetchAll = function() {
+        $.getJSON("data.json", function(data) {
+                if (localStorage.data) {
+                    localStorage.data = data;
 
-//sort projects by date published, newes first
-  allMyProjects.sort(function(a,b) {
-  return(new Date(b.publishedOn) - new Date(a.publishedOn));
-});
+                    //  console.log(data);
+                    Project.loadAll(data);
+                } })
+        }
 
-//iterate through the collection of all my projects (projectItems.js)
-//and create new Project instances, push them into projects[]
-  allMyProjects.forEach (function(project) {
-  projects.push(new Project (project));
-});
+        //sort projects by date published, newes first
+        Project.loadAll = function(allMyProjects) {
+            allMyProjects.sort(function(a, b) {
+                return (new Date(b.publishedOn) - new Date(a.publishedOn));
+            });
 
-//append each Project from the projects[] to the DOM
-projects.forEach (function(p) {
-  $('#projects').append(p.toHtml());
-});
+            //iterate through the collection of all my projects (projectItems.js)
+            //and create new Project instances, push them into projects[]
+            allMyProjects.forEach(function(project) {
+                projects.push(new Project(project));
+            });
+
+            //append each Project from the projects[] to the DOM
+            projects.forEach(function(p) {
+                $('#projects').append(p.toHtml());
+            });
+        }
